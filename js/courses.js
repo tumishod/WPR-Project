cf0ae075b0565014b2cf7dbb42059253809a5b1f
MSNkosi31 <142292604+MSNkosi31@users.noreply.github.com>
2024-08-01 18:24:27 +0200

    Reduction of modules, change in course names and small addition to the header (Not completed)

--- js/courses.js
+++ js/courses.js
@@ -83,9 +83,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Array containing course information, including NQF level
     const courses = [
-        { id: 1, title: 'Diploma', code: 'D456', duration: '2 years', NQFlevel: 'NQF level 6', description: 'Diploma in Information Technology' },
-        { id: 2, title: 'BIT', code: 'BIT789', duration: '3 years', NQFlevel: 'NQF level 7', description: 'Bachelor of Information Technology' },
-        { id: 3, title: 'BCOM', code: 'BCOM101', duration: '4 years', NQFlevel: 'NQF level 8', description: 'Bachelor of Computing' }
+        { id: 1, title: 'Diploma in Information Technology (DIT)', code: 'DIT456', duration: '2 years', NQFlevel: 'NQF level 6', description: 'Diploma in Information Technology' },
+        { id: 2, title: 'Bachelor in Information Technology (BIT)', code: 'BIT789', duration: '3 years', NQFlevel: 'NQF level 7', description: 'Bachelor of Information Technology' },
+        { id: 3, title: 'Bachelor in Information Technology (BCOM)', code: 'BCOM101', duration: '4 years', NQFlevel: 'NQF level 8', description: 'Bachelor of Computing' }
     ];
 
     // Object containing detailed course data, including years, modules, lecturers, and venue
@@ -93,8 +93,8 @@ document.addEventListener('DOMContentLoaded', function() {
         1: {
             title: 'Diploma',
             years: {
-                'First Year': ['BUC161', 'BME161', 'COA161', 'DBC161', 'DBF161', 'EUC161', 'INL161', 'IOT161', 'MAT161', 'NWD161', 'PRS161', 'PRG161', 'PRP161', 'WPR161', 'STA161'],
-                'Second Year': ['DBD261', 'ERP261', 'INL261', 'ILE261', 'PMM261', 'CNA261', 'IOT261', 'OPS261', 'OPS262', 'OPS263', 'SEC261', 'PRG261', 'PRG262', 'SWA261', 'SWT261', 'SWT262', 'UXD261', 'WPR261']
+                'First Year': ['BUC161', 'BME161', 'COA161', 'DBC161', 'DBF161'],
+                'Second Year': ['DBD261', 'ERP261', 'INL261', 'ILE261', 'PMM261']
             },
             lecturers: ['Lecturer1', 'Lecturer2', 'Lecturer3', 'Lecturer4', 'Lecturer5', 'Lecturer6', 'Lecturer7', 'Lecturer8', 'Lecturer9', 'Lecturer10', 'Lecturer11', 'Lecturer12', 'Lecturer13', 'Lecturer14', 'Lecturer15'],
             venue: ['Kempton Park', 'Pretoria', 'Online']
@@ -102,9 +102,9 @@ document.addEventListener('DOMContentLoaded', function() {
         2: {
             title: 'BIT',
             years: {
-                'First Year': ['ACW171', 'COA171', 'DBD171', 'ENG171', 'INF171', 'INL101', 'INL102', 'MAT171', 'NWD171', 'PRG171', 'PRG172', 'STA171', 'WPR171', 'BUM171', 'ENT171'],
-                'Second Year': ['CNA271', 'DBD221', 'ERP271', 'ETH271', 'INF271', 'INL201', 'INL202', 'LPR171', 'PRG271', 'PRG272', 'PMM271', 'STA271', 'WPR271', 'IOT271', 'SWT271'],
-                'Third Year': ['BIN371', 'CNA371', 'DAL371', 'DBD371', 'INL371', 'PRG371', 'PRJ371', 'PMM371', 'SAD371', 'SEN371', 'WPR371', 'INM371', 'UAX371']
+                'First Year': ['ACW171', 'COA171', 'DBD171', 'ENG171', 'INF171'],
+                'Second Year': ['CNA271', 'DBD221', 'ERP271', 'ETH271', 'INF271'],
+                'Third Year': ['BIN371', 'CNA371', 'DAL371', 'DBD371', 'INL371']
             },
             lecturers: ['Lecturer1', 'Lecturer2', 'Lecturer3', 'Lecturer4', 'Lecturer5', 'Lecturer6', 'Lecturer7', 'Lecturer8', 'Lecturer9', 'Lecturer10', 'Lecturer11', 'Lecturer12', 'Lecturer13', 'Lecturer14', 'Lecturer15'],
             venue: ['Kempton Park', 'Pretoria', 'Online']
@@ -112,9 +112,9 @@ document.addEventListener('DOMContentLoaded', function() {
         3: {
             title: 'BCOM',
             years: {
-                'First Year': ['ACW181', 'COA181', 'DBD181', 'INF181', 'INL101', 'INL102', 'LPR181', 'MAT181', 'NWD181', 'PRG181', 'PRG182', 'STA181', 'WPR181', 'BUM181', 'ENT181'],
-                'Second Year': ['DBD281', 'INF281', 'INL201', 'INL202', 'LPR281', 'MAT281', 'PRG281', 'PRG282', 'PMM281', 'STA281', 'WPR281', 'SAD281', 'DWH281', 'IOT281', 'SWT281'],
-                'Third Year': ['RSH381', 'DBD381', 'INL321', 'LPR381', 'MLG381', 'PRJ381', 'PMM381', 'PRG381', 'SEN381', 'WPR381', 'BIN381', 'DBA381', 'STA381', 'INM381', 'MLG382', 'UAX381'],
+                'First Year': ['ACW181', 'COA181', 'DBD181', 'INF181', 'INL101'],
+                'Second Year': ['DBD281', 'INF281', 'INL201', 'INL202', 'LPR281'],
+                'Third Year': ['RSH381', 'DBD381', 'INL321', 'LPR381', 'MLG381'],
                 'Fourth Year': ['AIT481', 'AIT482', 'DST481']
             },
             lecturers: ['Lecturer1', 'Lecturer2', 'Lecturer3', 'Lecturer4', 'Lecturer5', 'Lecturer6', 'Lecturer7', 'Lecturer8', 'Lecturer9', 'Lecturer10', 'Lecturer11', 'Lecturer12', 'Lecturer13', 'Lecturer14', 'Lecturer15'],
